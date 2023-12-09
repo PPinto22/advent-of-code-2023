d1eb8d4eed0aae82604c2d98701e892c8454160d
Pedro Pinto <pedropinto24@hotmail.com>
2023-12-09 23:47:02 +0000

    Add day 04

---
+++ day04/src/bin/part_a.rs
@@ -0,0 +1,78 @@
+use regex::Regex;
+use std::collections::HashSet;
+
+fn main() {
+    let input = include_str!("../../input.txt");
+    println!("{}", solve(input));
+}
+
+#[derive(Debug)]
+struct Card {
+    id: u32,
+    winning_numbers: HashSet<u32>,
+    owned_numbers: HashSet<u32>,
+}
+
+fn solve(input: &str) -> u32 {
+    let cards: Vec<Card> = input.lines().map(Card::parse).collect();
+    return cards.iter().map(|c| c.calculate_points()).sum();
+}
+
+impl Card {
+    fn parse(card_str: &str) -> Card {
+        let regex = Regex::new(r"^Card\s+(?<id>\d+): (?<winning_numbers>.*) \| (?<owned_numbers>.*)")
+            .unwrap();
+        let captures = regex.captures(card_str).unwrap();
+        let id = captures["id"].parse::<u32>().unwrap();
+        let winning_numbers: HashSet<u32> = captures["winning_numbers"]
+            .split_whitespace()
+            .map(|s| s.parse::<u32>().unwrap())
+            .collect();
+        let owned_numbers: HashSet<u32> = captures["owned_numbers"]
+            .split_whitespace()
+            .map(|s| s.parse::<u32>().unwrap())
+            .collect();
+
+        return Card {
+            id,
+            winning_numbers,
+            owned_numbers,
+        };
+    }
+
+    fn count_winning_numbers(&self) -> u32 {
+        return self
+            .winning_numbers
+            .intersection(&self.owned_numbers)
+            .count() as u32;
+    }
+
+    fn calculate_points(&self) -> u32 {
+        match self.count_winning_numbers() {
+            0 => 0,
+            count => u32::pow(2, count - 1),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::solve;
+    use indoc::indoc;
+
+    #[test]
+    fn solves_sample() {
+        let sample = indoc! {"
+            Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+            Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+            Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+            Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+            Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+            Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        "};
+
+        let solution = solve(sample);
+
+        assert_eq!(solution, 13);
+    }
+}
