395811f638f64e487ebbb5f324db840619b8973d
Pedro Pinto <pedropinto24@hotmail.com>
2023-12-26 19:21:31 +0000

    Add day 18

---
+++ day18/src/bin/part_1.rs
@@ -0,0 +1,186 @@
+use std::{
+    cmp::{max, min},
+    str::FromStr,
+};
+
+fn main() {
+    let input = include_str!("../../input.txt");
+    println!("{}", solve(input));
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+struct Point(usize, usize);
+
+#[derive(Debug)]
+struct Square {
+    left: i32,
+    right: i32,
+    top: i32,
+    bottom: i32,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Instruction {
+    direction: Direction,
+    meters: u32,
+}
+
+struct Polygon {
+    vertices: Vec<Point>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Direction {
+    R,
+    D,
+    L,
+    U,
+}
+
+fn solve(input: &str) -> u32 {
+    let instructions: Vec<Instruction> = input.lines().map(|l| l.parse().unwrap()).collect();
+    let dig_map = Polygon::from_instructions(&instructions);
+    dig_map.calculate_area()
+}
+
+impl Polygon {
+    fn from_instructions(instructions: &Vec<Instruction>) -> Self {
+        let square = Polygon::calculate_square_boundary(instructions);
+
+        let mut position = Point(square.top.abs() as usize, square.left.abs() as usize);
+        let mut vertices = vec![position.clone()];
+
+        for instruction in instructions {
+            position = Polygon::next_position(&position, instruction);
+            vertices.push(position.clone());
+        }
+
+        Polygon { vertices }
+    }
+
+    fn calculate_area(&self) -> u32 {
+        // shoelace formula
+        let double_interior_area: u32 = self
+            .vertices
+            .windows(2)
+            .map(|pair| (pair[0].0 * pair[1].1) as i32 - (pair[0].1 * pair[1].0) as i32)
+            .sum::<i32>()
+            .unsigned_abs();
+
+        let mut double_area = double_interior_area;
+
+        // edges
+        self.vertices.windows(2).for_each(|pair| {
+            double_area += (pair[0].0 as i32 - pair[1].0 as i32).unsigned_abs()
+                + (pair[0].1 as i32 - pair[1].1 as i32).unsigned_abs()
+        });
+
+        double_area / 2 + 1
+    }
+
+    fn calculate_square_boundary(instructions: &Vec<Instruction>) -> Square {
+        let mut left = i32::MAX;
+        let mut right = i32::MIN;
+        let mut top = i32::MAX;
+        let mut bottom = i32::MIN;
+
+        let mut cur_width: i32 = 0;
+        let mut cur_height: i32 = 0;
+
+        for instruction in instructions {
+            match instruction.direction {
+                Direction::R => {
+                    cur_width += instruction.meters as i32;
+                    right = max(right, cur_width);
+                }
+                Direction::L => {
+                    cur_width -= instruction.meters as i32;
+                    left = min(left, cur_width);
+                }
+                Direction::D => {
+                    cur_height += instruction.meters as i32;
+                    bottom = max(bottom, cur_height);
+                }
+                Direction::U => {
+                    cur_height -= instruction.meters as i32;
+                    top = min(top, cur_height);
+                }
+            }
+        }
+
+        Square {
+            left,
+            right,
+            top,
+            bottom,
+        }
+    }
+
+    fn next_position(position: &Point, instruction: &Instruction) -> Point {
+        let Point(i, j) = *position;
+
+        match instruction.direction {
+            Direction::R => Point(i, j + instruction.meters as usize),
+            Direction::D => Point(i + instruction.meters as usize, j),
+            Direction::L => Point(i, j - instruction.meters as usize),
+            Direction::U => Point(i - instruction.meters as usize, j),
+        }
+    }
+}
+
+impl FromStr for Instruction {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split_whitespace().collect();
+
+        Ok(Instruction {
+            direction: parts[0].parse().unwrap(),
+            meters: parts[1].parse().unwrap(),
+        })
+    }
+}
+
+impl FromStr for Direction {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "R" => Ok(Direction::R),
+            "D" => Ok(Direction::D),
+            "L" => Ok(Direction::L),
+            "U" => Ok(Direction::U),
+            other => panic!("Unknown direction: {other}"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::solve;
+    use indoc::indoc;
+
+    #[test]
+    fn solves_sample() {
+        let sample = indoc! {"
+            R 6 (#70c710)
+            D 5 (#0dc571)
+            L 2 (#5713f0)
+            D 2 (#d2c081)
+            R 2 (#59c680)
+            D 2 (#411b91)
+            L 5 (#8ceee2)
+            U 2 (#caa173)
+            L 1 (#1b58a2)
+            U 2 (#caa171)
+            R 2 (#7807d2)
+            U 3 (#a77fa3)
+            L 2 (#015232)
+            U 2 (#7a21e3)
+        "};
+
+        let solution = solve(sample);
+
+        assert_eq!(solution, 62);
+    }
+}
---
+++ day18/src/bin/part_2.rs
@@ -0,0 +1,183 @@
+use std::{
+    cmp::{max, min},
+    str::FromStr,
+};
+
+fn main() {
+    let input = include_str!("../../input.txt");
+    println!("{}", solve(input));
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+struct Point(i128, i128);
+
+#[derive(Debug)]
+struct Square {
+    left: i128,
+    right: i128,
+    top: i128,
+    bottom: i128,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Instruction {
+    direction: Direction,
+    meters: u128,
+}
+
+struct Polygon {
+    vertices: Vec<Point>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Direction {
+    R,
+    D,
+    L,
+    U,
+}
+
+fn solve(input: &str) -> u128 {
+    let instructions: Vec<Instruction> = input.lines().map(|l| l.parse().unwrap()).collect();
+    let dig_map = Polygon::from_instructions(&instructions);
+    dig_map.calculate_area()
+}
+
+impl Polygon {
+    fn from_instructions(instructions: &Vec<Instruction>) -> Self {
+        let square = Polygon::calculate_square_boundary(instructions);
+
+        let mut position = Point(square.top.abs(), square.left.abs());
+        let mut vertices = vec![position.clone()];
+
+        for instruction in instructions {
+            position = Polygon::next_position(&position, instruction);
+            vertices.push(position.clone());
+        }
+
+        Polygon { vertices }
+    }
+
+    fn calculate_area(&self) -> u128 {
+        // shoelace formula
+        let double_interior_area: u128 = self
+            .vertices
+            .windows(2)
+            .map(|pair| (pair[0].0 * pair[1].1) - (pair[0].1 * pair[1].0))
+            .sum::<i128>()
+            .unsigned_abs();
+
+        let mut double_area = double_interior_area;
+
+        // edges
+        self.vertices.windows(2).for_each(|pair| {
+            double_area +=
+                (pair[0].0 - pair[1].0).unsigned_abs() + (pair[0].1 - pair[1].1).unsigned_abs()
+        });
+
+        double_area / 2 + 1
+    }
+
+    fn calculate_square_boundary(instructions: &Vec<Instruction>) -> Square {
+        let mut left = i128::MAX;
+        let mut right = i128::MIN;
+        let mut top = i128::MAX;
+        let mut bottom = i128::MIN;
+
+        let mut cur_width: i128 = 0;
+        let mut cur_height: i128 = 0;
+
+        for instruction in instructions {
+            match instruction.direction {
+                Direction::R => {
+                    cur_width += instruction.meters as i128;
+                    right = max(right, cur_width);
+                }
+                Direction::L => {
+                    cur_width -= instruction.meters as i128;
+                    left = min(left, cur_width);
+                }
+                Direction::D => {
+                    cur_height += instruction.meters as i128;
+                    bottom = max(bottom, cur_height);
+                }
+                Direction::U => {
+                    cur_height -= instruction.meters as i128;
+                    top = min(top, cur_height);
+                }
+            }
+        }
+
+        Square {
+            left,
+            right,
+            top,
+            bottom,
+        }
+    }
+
+    fn next_position(position: &Point, instruction: &Instruction) -> Point {
+        let Point(i, j) = *position;
+
+        match instruction.direction {
+            Direction::R => Point(i, j + instruction.meters as i128),
+            Direction::D => Point(i + instruction.meters as i128, j),
+            Direction::L => Point(i, j - instruction.meters as i128),
+            Direction::U => Point(i - instruction.meters as i128, j),
+        }
+    }
+}
+
+impl FromStr for Instruction {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split_whitespace().collect();
+        let hex_digits = parts[2]
+            .strip_prefix("(#")
+            .unwrap()
+            .strip_suffix(")")
+            .unwrap();
+
+        let direction = match hex_digits.chars().last() {
+            Some('0') => Direction::R,
+            Some('1') => Direction::D,
+            Some('2') => Direction::L,
+            Some('3') => Direction::U,
+            _ => panic!("Unknown direction"),
+        };
+        let meters = u128::from_str_radix(&hex_digits[0..hex_digits.len() - 1], 16).unwrap();
+
+        Ok(Instruction { direction, meters })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::solve;
+    use indoc::indoc;
+
+    #[test]
+    fn solves_sample() {
+        let sample = indoc! {"
+            R 6 (#70c710)
+            D 5 (#0dc571)
+            L 2 (#5713f0)
+            D 2 (#d2c081)
+            R 2 (#59c680)
+            D 2 (#411b91)
+            L 5 (#8ceee2)
+            U 2 (#caa173)
+            L 1 (#1b58a2)
+            U 2 (#caa171)
+            R 2 (#7807d2)
+            U 3 (#a77fa3)
+            L 2 (#015232)
+            U 2 (#7a21e3)
+        "};
+
+        let solution = solve(sample);
+
+        assert_eq!(solution, 952408144115);
+    }
+}
